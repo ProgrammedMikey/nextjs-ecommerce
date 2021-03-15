ce56173598e1223b4e26b2a5b477b2aac1b34bd6
Michael Da Silva <programmedmikey@gmail.com>
2021-03-15 13:20:59 -0400

    setting up products page

---
+++ components/product/ProductItem.js
@@ -0,0 +1,50 @@
+import Link from 'next/link'
+
+const ProductItem = ({product}) => {
+    
+    const userLink = () => {
+        return(
+            <>
+                <Link href={`product/${product._id}`}>
+                    <a className="btn btn-info" 
+                    style={{ marginRight: '5px', flex:1 }}>View</a>
+                </Link>
+               <button className="btn btn-success"
+               style={{ marginLeft: '5px', flex:1 }}>
+                   Buy
+               </button>
+            </>
+        )
+    }
+    
+    return (
+        <div className="card" style={{ width: '18rem' }}>
+            <img className="card-img-top" src={product.images[0].url} alt={product.images[0].url} />
+            <div className="card-body">
+                <h5 className="card-title text-capitalize" title={product.title}>
+                    {product.title}
+                </h5> 
+                <div className="row justify-content-between mx-0">
+                    <h6 className="text-danger">${product.price}</h6>
+                    {
+                        product.inStock > 0 
+                        ? <h6 className="text-danger">In Stock: {product.inStock}</h6>
+                        : <h6 className="text-danger">Out of Stock</h6>
+                    }
+                </div>
+
+                <p className="card-text" title={product.description}> 
+                    {product.description}
+                </p>
+
+                <div className="row justify-content-between mx-0">
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+                    {useLink()}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default ProductItem
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,41 @@
-const Home = () => {
+import Head from 'next/head'
+import { useState } from 'react'
+import { getData } from '../utils/fetchData'
+import ProductItem from '../components/product/ProductItem'
+
+const Home = (props) => {
+  const [products, setProducts] = useState(props.products)
+
+
   return (
-    <div>Home</div>
+    <div className="products">
+      <Head>
+        <title>Home Page</title>
+      </Head>
+
+      {
+        products.length === 0 
+        ? <h2>No Products</h2>
+        : products.map(product => (
+          <ProductItem key={product._id} product={product} />
+        ))
+      }
+    </div> 
   )
+}
+
+export async function getServerSideProps(context) {
+  const res = await getData('product')
+  console.log(res)
+  //server side rendering
+
+
+  return {
+    props: {
+      products: res.products,
+      result: res.result
+    }, // will be passed to the page component as props
+  }
 }
 
 export default Home
