1829021505adc7c4f46c6568382c8d895276d2ef
Michael Da Silva <programmedmikey@gmail.com>
2021-03-31 14:54:53 -0400

    upload product image UI

--- pages/create/[[...id]].js
+++ pages/create/[[...id]].js
@@ -15,7 +15,10 @@ const ProductsManager = () => {
     const [product, setProduct] = useState(initialState)
     const {product_id, title, price, inStock, description, content, category} = product
 
+    [images, setImages] = useState([])
+
     const {state, dispatch} = useContext(DataContext)
+    const {categories} = state
 
     const handleChangeInput = e => {
         const {name, value} = e.target 
@@ -61,11 +64,46 @@ const ProductsManager = () => {
                     placeholder="Content" className="d-block my-4 w-100 p-2"
                     onChange={handleChangeInput} />
 
+                    <div className="input-group-prepend px-0 my-2">
+                        <select name="category" id="category" value={category}
+                        onChange={handleChangeInput} className="custom-select text-capitalize">
+                            <option value="all">All Products</option>
+                            {
+                                categories.map(item => (
+                                    <option key={item._id} value={item._id}>
+                                        {item.name}
+                                    </option>
+                                ))
+                            }
+                        </select>
+                    </div>
+
                 </div>
 
                 
-                <div className="col-md-6">
-                    
+                <div className="col-md-6 my-4">
+                    <div className="input-group mb-3">
+                        <div className="input-group-prepend">
+                            <span className="input-group-text">Upload</span>
+                        </div>
+
+                        <div className="custom-file border rounded">
+                            <input type="file" className="custom-file-input"
+                            onChange={handleUploadInput} multiple />
+                        </div>    
+                    </div>
+
+                    <div className="row img-up">
+                        {
+                            images.map((img, index) => (
+                                <div key={index} className="file_img">
+                                    <img src={img.url ? img.url : URL.createObjectURL(img)}
+                                     alt="" className="img-thumbnail rounded" />
+                                </div>
+                            ))
+                        }
+                    </div>    
+
                 </div>
             </form>
 
