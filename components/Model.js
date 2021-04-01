9dfc3dfe9520d0be9eb8448fbf33f21d30e00664
Michael Da Silva <programmedmikey@gmail.com>
2021-04-01 16:48:46 -0400

    delete or delete all products

--- components/Model.js
+++ components/Model.js
@@ -2,54 +2,85 @@ import { useContext } from 'react'
 import { DataContext } from '../store/GlobalState'
 import { deleteItem } from '../store/Actions'
 import { deleteData } from '../utils/fetchData'
+import {useRouter} from 'next/router'
+
 
 const Modal = () => {
     const {state, dispatch} = useContext(DataContext)
     const { modal, auth } = state
 
-    const handleSubmit = () => {
-        if(modal.type === 'ADD_USERS'){
-            deleteData(`user/${modal.id}`, auth.token)
-            .then(res => {
-                if(res.err) return dispatch({type: 'NOTIFY', payload: {error: res.err}})
-                return dispatch({type: 'NOTIFY', payload: {success: res.msg}})
-            })
-        }
+    const router = useRouter()
 
-        if(modal.type === 'ADD_CATEGORIES'){
-            deleteData(`categories/${modal.id}`, auth.token)
-            .then(res => {
-                if(res.err) return dispatch({type: 'NOTIFY', payload: {error: res.err}})
-                return dispatch({type: 'NOTIFY', payload: {success: res.msg}})
-            })
-        }
-
-        dispatch(deleteItem(modal.data, modal.id, modal.type))
-        dispatch({ type: 'ADD_MODAL', payload: {} })
+    const deleteUser = (item) => {
+        dispatch(deleteItem(item.data, item.id, item.type))
+        
+        deleteData(`user/${item.id}`, auth.token)
+        .then(res => {
+            if(res.err) return dispatch({type: 'NOTIFY', payload: {error: res.err}})
+            return dispatch({type: 'NOTIFY', payload: {success: res.msg}})
+        })
     }
-    
-    return (
-     <div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div className="modal-dialog" role="document">
-            <div className="modal-content">
+
+    const deleteCategories = (item) => {
+        deleteData(`categories/${item.id}`, auth.token)
+        .then(res => {
+            if(res.err) return dispatch({type: 'NOTIFY', payload: {error: res.err}})
+
+            dispatch(deleteItem(item.data, item.id, item.type))
+            return dispatch({type: 'NOTIFY', payload: {success: res.msg}})
+        })
+    }
+
+    const deleteProduct = (item) => {
+        dispatch({type: 'NOTIFY', payload: {loading: true}})
+        deleteData(`product/${item.id}`, auth.token)
+        .then(res => {
+            if(res.err) return dispatch({type: 'NOTIFY', payload: {error: res.err}})
+            dispatch({type: 'NOTIFY', payload: {success: res.msg}})
+            return router.push('/')
+        })
+    }
+
+    const handleSubmit = () => {
+        if(modal.length !== 0){
+            for(const item of modal){
+                if(item.type === 'ADD_CART'){
+                    dispatch(deleteItem(item.data, item.id, item.type))
+                }
+
+                if(item.type === 'ADD_USERS') deleteUser(item)
+        
+                if(item.type === 'ADD_CATEGORIES') deleteCategories(item)
+        
+                if(item.type === 'DELETE_PRODUCT') deleteProduct(item)
+        
+                dispatch({ type: 'ADD_MODAL', payload: [] })
+            }
+        }
+    }
+
+    return(
+        <div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div className="modal-dialog" role="document">
+                <div className="modal-content">
                 <div className="modal-header">
                     <h5 className="modal-title text-capitalize" id="exampleModalLabel">
-                        {modal.title}
+                        {modal.length !== 0 && modal[0].title}
                     </h5>
                     <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
+                    <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
                 <div className="modal-body">
-                    Are you sure you want to delete this item?
+                    Do you want to delete this item?
                 </div>
                 <div className="modal-footer">
                     <button type="button" className="btn btn-secondary" data-dismiss="modal" onClick={handleSubmit}>Yes</button>
                     <button type="button" className="btn btn-primary" data-dismiss="modal">Cancel</button>
                 </div>
+                </div>
             </div>
         </div>
-      </div>
     )
 }
 
--- store/GlobalState.js
+++ store/GlobalState.js
@@ -41,13 +41,13 @@ export const DataProvider = ({children}) => {
     },[])
 
     useEffect(() => {
-       const __next__cart01 = JSON.parse(localStorage.getItem('__next__cart01'))
+        const __next__cart01__devat = JSON.parse(localStorage.getItem('__next__cart01__devat'))
 
-       if(__next__cart01) dispatch({ type: 'ADD_CART', payload: __next__cart01 })
+        if(__next__cart01__devat) dispatch({ type: 'ADD_CART', payload: __next__cart01__devat })
     }, [])
 
     useEffect(() => {
-        localStorage.setItem('__next__cart01', JSON.stringify(cart))
+        localStorage.setItem('__next__cart01__devat', JSON.stringify(cart))
     }, [cart])
 
     useEffect(() => {
@@ -55,26 +55,26 @@ export const DataProvider = ({children}) => {
             getData('order', auth.token)
             .then(res => {
                 if(res.err) return dispatch({type: 'NOTIFY', payload: {error: res.err}})
-
+                
                 dispatch({type: 'ADD_ORDERS', payload: res.orders})
             })
 
             if(auth.user.role === 'admin'){
-                getData('user', auth.token) 
+                getData('user', auth.token)
                 .then(res => {
                     if(res.err) return dispatch({type: 'NOTIFY', payload: {error: res.err}})
-
+                
                     dispatch({type: 'ADD_USERS', payload: res.users})
                 })
             }
-        } else {
+        }else{
             dispatch({type: 'ADD_ORDERS', payload: []})
             dispatch({type: 'ADD_USERS', payload: []})
         }
-    }, [auth.token])
+    },[auth.token])
 
-    return (
-        <DataContext.Provider value={{ state, dispatch }}>
+    return(
+        <DataContext.Provider value={{state, dispatch}}>
             {children}
         </DataContext.Provider>
     )
